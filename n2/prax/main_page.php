653b18981a776e986aaa8e268743c7217ea7aebd
rturi <rolandtyri@gmail.com>
2016-02-28 09:43:12 +0200

    kt2 - week 2 prax

---
+++ n2/prax/main_page.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>I244 prax</title>
+</head>
+    <!-- http://enos.itcollege.ee/~rturi/I244/n2/prax/main_page.php-->
+    <?php include("navigation.php"); ?>
+
+    <h1>This is the main page</h1>
+    <p>The mainest place this side of Mainville ME</p>
+
+</body>
+</html>
