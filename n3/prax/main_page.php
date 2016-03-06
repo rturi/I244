2df145c919f21295fa7054bb81021191aaa23e97
rturi <rolandtyri@gmail.com>
2016-03-06 11:02:32 +0200

    week 3 - prax 3 clearfix

--- n3/prax/main_page.php
+++ n3/prax/main_page.php
@@ -8,9 +8,6 @@
 
 
 <!--
-Lisada lehel olevale pealkirjale sobiv värv (color).
-Selleks, et kõik selle taseme pealkirjad samat värvi ei oleks anna värvitavale pealkirja elemendile id atribuut ning kasuta css-is selektorit #id_väärtus mis viitab konkreetsele id atribuudi väärtusele.
-Värvi saab kirjutada kas #HEX NUMBRID või rgb(decimal numbrid). Näiteks #F8F8FF ja rgb(248,248,255) on ekvivalentsed Värvikaardi leiad siit
 ümbritseda lehe sisu (kõik body vahel) div-iga, millel on lehe taustavärvist erinev taustavärv. Selleks, et kõikidel tulevastel div elementidel poleks sama taustavärv, tuleb elemendile anda kas klass või id
 ümbritseda lehe reaalne sisu (va. menüülinke sisaldav list ja validaatori logo) omakorda div elemendiga. kirjelda sellele lehe sisu hõlmavale div-ile järgmine kujundus:
 taustavärv valge (#ffffff) VÕI mingi oma valitav värvus
@@ -35,10 +32,12 @@ Selleks, et ka teistel vaadetel (galerii, sisse logimine jne) oleks sarnane väl
 <body>
 
 <div class="page_container">
+
+    <!-- Menu generated with PHP: -->
     <?php include("navigation.php"); ?>
 
     <div class="content">
-        <h1>Tähtis pealkiri</h1>
+        <h1 id="top_h1">Tähtis pealkiri</h1>
         <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
             magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
@@ -81,18 +80,20 @@ Selleks, et ka teistel vaadetel (galerii, sisse logimine jne) oleks sarnane väl
             magna aliqua:
         </p>
             <pre>
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                laboris nisi ut aliquip ex ea commodo consequat.
+                Duis aute irure dolor in reprehenderit in voluptate velit
+                esse cillum dolore eu fugiat nulla pariatur.
             </pre>
     </div>
 
 
-</div>
+    <div class="validator">
+        <a href="http://validator.w3.org/check?uri=referer">
+            <img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="31" width="88" />
+        </a>
+    </div>
 
-<div class="validator">
-    <a href="http://validator.w3.org/check?uri=referer">
-        <img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="31" width="88" />
-    </a>
 </div>
 
 </body>
