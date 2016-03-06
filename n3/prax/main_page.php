63b307c3efdb3eb3801847b3958add9eba73d2d1
rturi <rolandtyri@gmail.com>
2016-03-06 18:55:28 +0200

    week 3 - prax 3 completed

--- n3/prax/main_page.php
+++ n3/prax/main_page.php
@@ -6,29 +6,6 @@
     <link rel="stylesheet" href="style.css">
 </head>
 
-
-<!--
-ümbritseda lehe sisu (kõik body vahel) div-iga, millel on lehe taustavärvist erinev taustavärv. Selleks, et kõikidel tulevastel div elementidel poleks sama taustavärv, tuleb elemendile anda kas klass või id
-ümbritseda lehe reaalne sisu (va. menüülinke sisaldav list ja validaatori logo) omakorda div elemendiga. kirjelda sellele lehe sisu hõlmavale div-ile järgmine kujundus:
-taustavärv valge (#ffffff) VÕI mingi oma valitav värvus
-teksti joondus justify
-anna menüü linke sisaldavale listielemendile (ul või ol) id atribuut mingi väärtusega (nt menu).
-Muuta menüülinkide stiili (kasutada nt: color, text-decoration, font-weight, font-size jne.). Kasutada id-ga vanema sees oleva lingi selektorit piiramaks stiili mõju menüü sees olevatele linkidele
-CSS võimaldab reageerida hiire kursori liikumisele elemendi kohale. Selleks lisatakse elemendi selektori järgi :hover ning määratakse stiilireeglid, mida rakendada hiirega konkreetse elemendi kohal olles. Enamasti kasutatakse seda linkide stiili muutmiseks, aga ka keerukamate menüüde loomisel (google - css menu)
-Teha nii, et hiirega menüüs oleva lingi kohale minnes, muudab see oma stiili (taustavärv, tekstivärv, tekib/kaob alljoon etc.)
-
-Organiseerida nii, et piltidel on piirjoon (border) ümber, aga validaatori nupul seda ei ole.
-Vihje: kirjelda stiil ainult kindla div-i sees olevatele img elementidele või anna piltidele class atribuut.
-Kui kasutad windowsi, saad kontrollida kas IE-s validaatori logol on piirjoon või mitte. Selle saab eemaldada andes validaatori logole omaduse border:none;
-Kontrolli taaskord, et (http://validator.w3.org) on ikka veel rõõmus ja roheline, seda saad teha lihtsalt: kliki lehel olevale validaatori logo pildile.
-Lisaks saad kontrollida CSS õigsust CSS validaatoris. Edaspidiseks mugavaks CSS valideerimiseks võid (aga ei pea) pealeht.html HTML validaatori logo juurde lisada ka CSS validaatori logo:
-<a href="http://jigsaw.w3.org/css-validator/check/referer">
-  <img src="http://jigsaw.w3.org/css-validator/images/vcss" alt="Valid CSS!" />
-</a>
-Selleks, et ka teistel vaadetel (galerii, sisse logimine jne) oleks sarnane välimus, tuleks ka neile samad muudatused sisse viia...
-
--->
-
 <body>
 
 <div class="page_container">
@@ -80,10 +57,10 @@ Selleks, et ka teistel vaadetel (galerii, sisse logimine jne) oleks sarnane väl
             magna aliqua:
         </p>
             <pre>
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                laboris nisi ut aliquip ex ea commodo consequat.
-                Duis aute irure dolor in reprehenderit in voluptate velit
-                esse cillum dolore eu fugiat nulla pariatur.
+Ut enim ad minim veniam, quis nostrud exercitation ullamco
+laboris nisi ut aliquip ex ea commodo consequat.
+Duis aute irure dolor in reprehenderit in voluptate velit
+esse cillum dolore eu fugiat nulla pariatur.
             </pre>
     </div>
 
