3423aeddf835d69eb53038e24d75eb5ac4742855
rturi <rolandtyri@gmail.com>
2016-06-12 13:19:24 +0300

    Eksam - esimene testcommit

---
+++ Eksam/index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+Loo lihtne kommentaaride lisamise vorm. Andmed salvesta tekstifaili. Kuva salvestatud kommentaare.
+Lahendust mõeldes eelda, et kasutaja brauseris on lubatud nii Javascript kui ka küpsised.
+ */
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+test
+</body>
+</html>
+
+
